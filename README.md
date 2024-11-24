5eaae732c198a2140d9f7a2d2a66b24b0b6c9343
Stelios <parasirakisst@outlook.com.gr>
2024-11-24 15:13:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## GSID Fix
+Since Andrioid 14 QPR3 GSID will not work devices with unencrypted /data, meaning you can no longer use DSUs. This Magisk module replaces the gsid binary with an older version.
---
+++ config.sh
@@ -0,0 +1,52 @@
+# This will be the folder name under /magisk
+# This should also be the same as the id in your module.prop to prevent confusion
+MODID=template
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "          GSID FIX             "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+# NOTE: This part has to be adjusted to fit your own needs
+
+set_permissions() {
+  # Default permissions, don't remove them
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Some templates if you have no idea what to do:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+}
