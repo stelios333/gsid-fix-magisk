cd3bfb408480fb67bf904460f1c9eb81657340e7
Stelios <parasirakisst@outlook.com.gr>
2024-11-25 21:00:22 +0200

    Fix installation issues

--- customize.sh
+++ customize.sh
@@ -1,2 +1,8 @@
+print_modname() {
+  ui_print "*******************************"
+  ui_print "          GSID Fix             "
+  ui_print "*******************************"
+}
+
 echo "- Setting permissions and SELinux contexts..."
 set_perm $MODPATH/system/bin/gsid 0 0 0700 u:object_r:gsid_exec:s0
